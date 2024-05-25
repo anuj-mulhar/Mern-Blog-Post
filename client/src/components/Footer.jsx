dc656614b390775449f35c980c7b14b0bf762dfb
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-05-26 00:14:49 +0530

    create and add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitterX, BsGithub, BsLinkedin} from 'react-icons/bs';
+
+export default function FooterCom() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg  sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white    ">
+                Anuj's
+              </span>
+              Blog
+            </Link>
+          </div>
+
+          <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+
+            <div>
+            <Footer.Title title="About" />
+            <Footer.LinkGroup col>
+                <Footer.Link 
+                href='https://www.100jsprojects.com'
+                target="_blank" 
+                rel = 'noopener noreferrer'
+                >
+                    100 JS Projects
+                </Footer.Link>
+                <Footer.Link 
+                href='/about'
+                target="_blank" 
+                rel = 'noopener noreferrer'
+                >
+                    Anuj's Blog
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+            
+            <div>
+            <Footer.Title title="Follow Us" />
+            <Footer.LinkGroup col>
+                <Footer.Link 
+                href='https://github.com/anuj-mulhar'
+                target="_blank" 
+                rel = 'noopener noreferrer'
+                >
+                    Github
+                </Footer.Link>
+                <Footer.Link 
+                href='https://www.linkedin.com/in/anuj-mulhar-a294b5231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"'
+                target="_blank" 
+                rel = 'noopener noreferrer'
+                >
+                    LinkedIn
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+
+            <div>
+            <Footer.Title title="Legal" />
+            <Footer.LinkGroup col>
+                <Footer.Link 
+                href='#'
+                >
+                    Privacy Policy
+                </Footer.Link>
+                <Footer.Link 
+                href='#'
+                >
+                  Terms &amp; Conditions
+                </Footer.Link>
+            </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+            <Footer.Copyright href="#" by="Anuj's Blog" year={new Date().getFullYear()}/>
+            <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                <Footer.Icon href="#" icon={BsFacebook}/>
+                <Footer.Icon href="#" icon={BsInstagram}/>
+                <Footer.Icon href="#" icon={BsTwitterX}/>
+                <Footer.Icon href="#" icon={BsGithub}/>
+                <Footer.Icon href="#" icon={BsLinkedin}/>
+
+            </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
