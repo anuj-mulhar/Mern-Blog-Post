ffd30787c16f8c4590587e10d4a861b7f99623dc
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-06-02 23:07:05 +0530

    show users to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Sidebar } from "flowbite-react";
-import { HiArrowSmRight, HiDocumentText, HiUser } from "react-icons/hi";
+import { HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -59,6 +59,17 @@ export default function DashSidebar() {
               as="div"
             >
               Posts
+            </Sidebar.Item>
+          </Link>
+          )}
+          {currentUser.isAdmin && (
+          <Link to="/dashboard?tab=users">
+            <Sidebar.Item
+              active={tab === "users"}
+              icon={HiOutlineUserGroup}
+              as="div"
+            >
+              Users
             </Sidebar.Item>
           </Link>
           )}
