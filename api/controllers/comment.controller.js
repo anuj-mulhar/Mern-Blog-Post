8fee7f102c75dd17501654da1fc7f09ca3ba4717
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-06-03 22:55:35 +0530

    show the comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -4,7 +4,7 @@ export const createComment = async (req, res, next) => {
     try {
         const { content, postId, userId } = req.body;
         if(userId !== req.user.id){
-            return next(errorHandler(403, 'You are not allowed to create this comment'))
+            return next(errorHandler(403, 'You are not allowed to create this comment'));
         }
 
         const newComment = new Comment({
@@ -16,6 +16,17 @@ export const createComment = async (req, res, next) => {
 
         res.status(200).json(newComment);
 
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getPostComments = async (req, res, next) => {
+    try {
+        const comments = await Comment.find({ postId: req.params.postId }).sort({
+            createdAt: -1,
+        });
+        res.status(200).json(comments);
     } catch (error) {
         next(error);
     }
