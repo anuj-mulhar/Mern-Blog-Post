b1c98d1f232d52c3bea14f01275d4c61b5191ffc
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-06-04 12:19:12 +0530

    add edit functionality to the comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,6 +1,5 @@
 import Comment from '../models/comment.model.js';
 
-
 export const createComment = async (req, res, next) => {
     try {
         const { content, postId, userId } = req.body;
@@ -49,6 +48,28 @@ export const likeComment = async (req, res, next) => {
         }
         await comment.save();
         res.status(200).json(comment);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const editComment = async (req, res, next ) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next (errorHandler(404, "Comment not found"));
+        }
+        if (comment.userId !== req.user.id && !req.user.isAdmin) {
+            return next(errorHandler(403, 'You are not allow to edit this comment'));
+        }
+        const editedComment = await Comment.findByIdAndUpdate(
+            req.params.commentId,
+            {
+                content: req.body.content,
+            },
+            { new: true}
+        );
+        res.status(200).json(editedComment);
     } catch (error) {
         next(error);
     }
