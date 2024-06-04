bab546603c201f1ff88468672057e9379c5c89a8
anuj-mulhar <mulharanujsuresh123@gmail.com>
2024-06-05 00:48:46 +0530

    complete search  page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
@@ -10,9 +10,21 @@ import { signoutSuccess } from "../redux/user/userSlice";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const location = useLocation();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [searchTerm, setSearchTerm] = useState('');
+  
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   
   const handleSignout = async () => {
     try {
@@ -29,6 +41,16 @@ export default function Header() {
       console.log(error.message);
     }
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+
+
+  }
   return (
     <Navbar className="border-b-2">
       <Link
@@ -40,12 +62,15 @@ export default function Header() {
         </span>
         Blog
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type="text"
           placeholder="Search.."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -129,10 +129,41 @@ export default function UpdatePost() {
             }
             value={formData.category}
           >
-            <option value='uncategorized'>Select a category</option>
-            <option value='javascript'>JavaScript</option>
-            <option value='reactjs'>React.js</option>
-            <option value='nextjs'>Next.js</option>
+            <option value="uncategorized">Select a category</option>
+            <option value="html">HTML</option>
+            <option value="css">CSS</option>
+            <option value="javascript">JavaScript</option>
+            <option value="sql">SQL</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+            <option value="php">Php</option>
+            <option value="c">C </option>
+            <option value="c++">C++ </option>
+            <option value="c#">C# </option>
+            <option value="bootstrap">Bootstrap </option>
+            <option value="react">React </option>
+            <option value="mysql">MySQL </option>
+            <option value="jquery">JQuery </option>
+            <option value="django">Django </option>
+            <option value="numpy">Numpy </option>
+            <option value="pandas">Pandas </option>
+            <option value="nodejs">NodeJS </option>
+            <option value="r">R </option>
+            <option value="typescript">Typescript</option>
+            <option value="angular"> Angular</option>
+            <option value="git">Git </option>
+            <option value="postgresql">PostgreSQL </option>
+            <option value="mongodb"> MongoDB</option>
+            <option value="asp">ASP </option>
+            <option value="go"> GO</option>
+            <option value="kotlin"> Kotlin</option>
+            <option value="vue">VueJS </option>
+            <option value="dsa"> DSA</option>
+            <option value="genai"> GENAI</option>
+            <option value="scipy"> SCIPY</option>
+            <option value="aws"> AWS</option>
+            <option value="datascience"> Data Science</option>
+            <option value="cybersecurity"> Cybersecurity</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
